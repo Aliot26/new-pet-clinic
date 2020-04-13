2fea0eb62e25356f441e7dc20cc73ef8d29e1067
Aliot26 <olgakochanowa@gmail.com>
2020-04-13 20:54:50 +0200

    Added POJOs. Closes #2.

---
+++ src/main/java/volha/spring/newpetclinic/model/PetType.java
@@ -0,0 +1,16 @@
+package volha.spring.newpetclinic.model;
+
+/*
+ *Created by olga on 13.04.2020
+ */
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
