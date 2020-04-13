65fc0688f9caf6c0928d419fa32183f81f3bf0d8
Aliot26 <olgakochanowa@gmail.com>
2020-04-13 15:20:51 +0200

    Initial commit

---
+++ src/main/java/volha/spring/newpetclinic/NewPetClinicApplication.java
@@ -0,0 +1,13 @@
+package volha.spring.newpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/volha/spring/newpetclinic/NewPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package volha.spring.newpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
