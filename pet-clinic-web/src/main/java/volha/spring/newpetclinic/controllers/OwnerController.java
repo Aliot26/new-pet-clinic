9a1290c207dbfbb131f5a9e3d5ff15436cee4fb4
Aliot26 <olgakochanowa@gmail.com>
2020-04-16 17:13:01 +0200

    Added Owner's list and updated OwnerController. Closes #16.

--- pet-clinic-web/src/main/java/volha/spring/newpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/volha/spring/newpetclinic/controllers/OwnerController.java
@@ -5,12 +5,22 @@ package volha.spring.newpetclinic.controllers;
  */
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import volha.spring.newpetclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
