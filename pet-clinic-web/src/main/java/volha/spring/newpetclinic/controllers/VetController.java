b74043a42ac340f69310e2fb4610d54a714058a5
Aliot26 <olgakochanowa@gmail.com>
2020-04-16 10:41:24 +0200

    Added vets page and VetController. Closes #12.

---
+++ pet-clinic-web/src/main/java/volha/spring/newpetclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package volha.spring.newpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ *Created by olga on 16.04.2020
+ */
+@Controller
+public class VetController {
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
