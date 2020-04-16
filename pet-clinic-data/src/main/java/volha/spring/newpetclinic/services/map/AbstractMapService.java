ab55736b3721bf6897748ef9085f969d29c622a6
Aliot26 <olgakochanowa@gmail.com>
2020-04-16 19:40:07 +0200

    Refactored Map Impl to abstract ID generation. Closes #18

--- pet-clinic-data/src/main/java/volha/spring/newpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/volha/spring/newpetclinic/services/map/AbstractMapService.java
@@ -1,34 +1,50 @@
 package volha.spring.newpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import volha.spring.newpetclinic.model.BaseEntity;
+
+import java.util.*;
 
 /*
  *Created by olga on 15.04.2020
  */
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
-    Set<T> findAll(){
+    Set<T> findAll() {
         return new HashSet<>(map.values());
     }
 
-    T findById(ID id){
+    T findById(ID id) {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
-    void deleteById(ID id){
+    void deleteById(ID id) {
         map.remove(id);
     }
 
-    void delete(T object){
-        map.entrySet().removeIf(entry->entry.getValue().equals(object));
+    void delete(T object) {
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
