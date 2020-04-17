eacc509f0af67b154ff9c65ad7dd9daed9eca068
Aliot26 <olgakochanowa@gmail.com>
2020-04-17 18:46:11 +0200

    Created Speciality entity. Closes #24.

---
+++ pet-clinic-data/src/main/java/volha/spring/newpetclinic/model/Speciality.java
@@ -0,0 +1,7 @@
+package volha.spring.newpetclinic.model;
+
+/*
+ *Created by olga on 17.04.2020
+ */
+public class Speciality extends BaseEntity {
+}
--- pet-clinic-data/src/main/java/volha/spring/newpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/volha/spring/newpetclinic/model/Vet.java
@@ -1,7 +1,18 @@
 package volha.spring.newpetclinic.model;
 
+import java.util.Set;
+
 /*
  *Created by olga on 13.04.2020
  */
 public class Vet extends Person {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
