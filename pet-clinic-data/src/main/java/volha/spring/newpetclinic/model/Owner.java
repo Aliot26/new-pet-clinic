4071b6414df70b2036a568dc84b6d9f6456c8900
Aliot26 <olgakochanowa@gmail.com>
2020-04-17 20:03:26 +0200

    Added contact info properties to owner. Closes #25.

--- pet-clinic-data/src/main/java/volha/spring/newpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/volha/spring/newpetclinic/model/Owner.java
@@ -6,5 +6,40 @@ import java.util.Set;
  *Created by olga on 13.04.2020
  */
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
