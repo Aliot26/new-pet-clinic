2a426253da736a3eab6560ea3a65ff9f502e168f
Aliot26 <olgakochanowa@gmail.com>
2020-04-22 20:33:09 +0200

    Implemented VisitMapService. Closes #44.

---
+++ pet-clinic-data/src/main/java/volha/spring/newpetclinic/services/VisitServise.java
@@ -0,0 +1,9 @@
+package volha.spring.newpetclinic.services;
+
+import volha.spring.newpetclinic.model.Visit;
+
+/*
+ *Created by olga on 22.04.2020
+ */
+public interface VisitServise extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/volha/spring/newpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,45 @@
+package volha.spring.newpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import volha.spring.newpetclinic.model.Visit;
+import volha.spring.newpetclinic.services.VisitServise;
+
+import java.util.Set;
+
+/*
+ *Created by olga on 22.04.2020
+ */
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitServise {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null
+                || visit.getPet().getOwner() == null
+                || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid visit");
+        }
+
+            return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
