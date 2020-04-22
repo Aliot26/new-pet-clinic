ae7a0e05257d4b39dd5966687c6d5fd3cc1914e8
Aliot26 <olgakochanowa@gmail.com>
2020-04-22 15:36:51 +0200

    Implemented PetJpaService. Closes #41.

---
+++ pet-clinic-data/src/main/java/volha/spring/newpetclinic/services/springdatajpa/PetJpaService.java
@@ -0,0 +1,50 @@
+package volha.spring.newpetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import volha.spring.newpetclinic.model.Pet;
+import volha.spring.newpetclinic.repositories.PetRepository;
+import volha.spring.newpetclinic.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ *Created by olga on 22.04.2020
+ */
+@Service
+@Profile("springdatajpa")
+public class PetJpaService implements PetService {
+    private final PetRepository petRepository;
+
+    public PetJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
