22688942cc9f27b24c4dcd8a616ee4fed3a30b48
Aliot26 <olgakochanowa@gmail.com>
2020-04-24 11:30:09 +0200

    Implemented VisitJpaService. Closes #45.

---
+++ pet-clinic-data/src/main/java/volha/spring/newpetclinic/services/springdatajpa/VisitJpaService.java
@@ -0,0 +1,50 @@
+package volha.spring.newpetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import volha.spring.newpetclinic.model.Visit;
+import volha.spring.newpetclinic.repositories.VisitRepository;
+import volha.spring.newpetclinic.services.VisitServise;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ *Created by olga on 24.04.2020
+ */
+@Service
+@Profile("springdatajpa")
+public class VisitJpaService implements VisitServise {
+    private final VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
