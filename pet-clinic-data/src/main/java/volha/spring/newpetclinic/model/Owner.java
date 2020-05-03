e86941b9bf9d3c0f16db8ddcc7cdd97e6bcec40b
Aliot26 <olgakochanowa@gmail.com>
2020-05-03 20:23:01 +0200

    Added method return Pet with given name

--- pet-clinic-data/src/main/java/volha/spring/newpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/volha/spring/newpetclinic/model/Owner.java
@@ -38,4 +38,34 @@ public class Owner extends Person {
         this.telephone = telephone;
         this.pets = pets;
     }
+
+    /**
+     * Return the Pet with the given name, or null if none found for this Owner.
+     *
+     * @param name to test
+     * @return true if pet name is already in use
+     */
+    public Pet getPet(String name) {
+        return getPet(name, false);
+    }
+
+    /**
+     * Return the Pet with the given name, or null if none found for this Owner.
+     *
+     * @param name to test
+     * @return true if pet name is already in use
+     */
+    public Pet getPet(String name, boolean ignoreNew) {
+        name = name.toLowerCase();
+        for (Pet pet : pets) {
+            if (!ignoreNew || !pet.isNew()) {
+                String compName = pet.getName();
+                compName = compName.toLowerCase();
+                if (compName.equals(name)) {
+                    return pet;
+                }
+            }
+        }
+        return null;
+    }
 }
