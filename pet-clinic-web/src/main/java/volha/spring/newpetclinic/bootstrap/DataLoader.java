1685f7bde229e1d79af937dd479eec880804630f
Aliot26 <olgakochanowa@gmail.com>
2020-07-06 18:14:52 +0200

    added sql file with data

--- pet-clinic-web/src/main/java/volha/spring/newpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/volha/spring/newpetclinic/bootstrap/DataLoader.java
@@ -2,48 +2,40 @@ package volha.spring.newpetclinic.bootstrap;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import volha.spring.newpetclinic.model.*;
-import volha.spring.newpetclinic.services.*;
-
-import java.time.LocalDate;
+import volha.spring.newpetclinic.model.PetType;
+import volha.spring.newpetclinic.model.Speciality;
+import volha.spring.newpetclinic.services.PetTypeService;
+import volha.spring.newpetclinic.services.SpecialityService;
 
 /*
  *Created by olga on 16.04.2020
  */
 @Component
-public class DataLoader implements CommandLineRunner{
-    private final OwnerService ownerService;
-    private final VetService vetService;
+public class DataLoader implements CommandLineRunner {
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
-    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService, VisitService visitService) {
-        this.ownerService = ownerService;
-        this.vetService = vetService;
+    public DataLoader(PetTypeService petTypeService, SpecialityService specialityService) {
+
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
-        this.visitService = visitService;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
-        int count = petTypeService.findAll().size();
-        if (count == 0) {
-            loadData();
+        int countTypes = petTypeService.findAll().size();
+        if (countTypes == 0) {
+            loadTypes();
+        }
+
+        int countSpecialities = specialityService.findAll().size();
+        if (countSpecialities == 0) {
+            loadSpecialities();
         }
     }
 
-    private void loadData() {
-        PetType dog = new PetType();
-        dog.setName("Dog");
-        PetType savedDogPetType = petTypeService.save(dog);
-
-        PetType cat = new PetType();
-        cat.setName("Cat");
-        PetType savedCatPetType = petTypeService.save(cat);
-
+    private void loadSpecialities() {
         Speciality radiology = new Speciality();
         radiology.setDescription("Radiology");
         Speciality savedRadiology = specialityService.save(radiology);
@@ -56,63 +48,16 @@ public class DataLoader implements CommandLineRunner{
         dentistry.setDescription("Dentistry");
         Speciality savedDentistry = specialityService.save(dentistry);
 
-        Owner owner1 = new Owner();
-        owner1.setFirstName("Michael");
-        owner1.setLastName(("Weston"));
-        owner1.setAddress("Zlota 20");
-        owner1.setCity("Warsaw");
-        owner1.setTelephone("1234566");
 
-//        Owner.builder().address("AAASSDD").id(3L).firstName("Adam").build();
+    }
 
-        Pet mikesPet = new Pet();
-        mikesPet.setPetType(savedDogPetType);
-        mikesPet.setOwner(owner1);
-        mikesPet.setBirthDate(LocalDate.now());
-        mikesPet.setName("Jekkie");
-        owner1.getPets().add(mikesPet);
+    private void loadTypes() {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
 
-        ownerService.save(owner1);
-
-        Owner owner2 = new Owner();
-        owner2.setFirstName("Fiona");
-        owner2.setLastName("Black");
-        owner2.setAddress("Zelazna 2");
-        owner2.setCity("Warsaw");
-        owner2.setTelephone("7234522");
-
-        Pet fionasPet = new Pet();
-        fionasPet.setPetType(savedCatPetType);
-        fionasPet.setOwner(owner2);
-        fionasPet.setBirthDate(LocalDate.now());
-        fionasPet.setName("Jerry");
-        owner2.getPets().add(fionasPet);
-
-        ownerService.save(owner2);
-
-        System.out.println("Loaded owners....");
-
-//        Visit catVisit = new Visit();
-//        catVisit.setPet(fionasPet);
-//        catVisit.setDate(LocalDate.now());
-//        catVisit.setDescription("Sneezy Kitty");
-//
-//        visitService.save(catVisit);
-
-        Vet vet1 = new Vet();
-        vet1.setFirstName("Alex");
-        vet1.setLastName("Norman");
-        vet1.getSpecialities().add(savedRadiology);
-
-        vetService.save(vet1);
-
-        Vet vet2 = new Vet();
-        vet2.setFirstName("Diana");
-        vet2.setLastName("Axe");
-        vet2.getSpecialities().add(savedSurgery);
-
-        vetService.save(vet2);
-
-        System.out.println("Loaded vets....");
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
     }
 }
