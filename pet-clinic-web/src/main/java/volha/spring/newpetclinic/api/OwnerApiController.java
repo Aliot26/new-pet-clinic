7cc22760b91bdd3a93c1297b028d7a5f1748c247
Aliot26 <olgakochanowa@gmail.com>
2020-08-12 17:42:42 +0200

    added SwaggerConfig

--- pet-clinic-web/src/main/java/volha/spring/newpetclinic/api/OwnerApiController.java
+++ pet-clinic-web/src/main/java/volha/spring/newpetclinic/api/OwnerApiController.java
@@ -1,8 +1,12 @@
 package volha.spring.newpetclinic.api;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import volha.spring.newpetclinic.model.Owner;
 import volha.spring.newpetclinic.services.OwnerService;
@@ -12,17 +16,24 @@ import java.util.Set;
 /*
  *Created by olga on 11.08.2020
  */
+@Api("OwnerController for aplication Pet Clinic")
 @RestController
+@RequestMapping("/api")
 public class OwnerApiController{
     private final OwnerService ownerService;
 
     public OwnerApiController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
-
-    @GetMapping({"/api/owners"})
+    @ApiOperation(value = "This will get a list of owners", notes = "In this list we have pets")
+    @GetMapping("/owners")
     public ResponseEntity<Set<Owner>> getOwners(){
         return new ResponseEntity<Set<Owner>>(ownerService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/owners/{owner_id}")
+    public ResponseEntity<Owner> getOwnerById(@PathVariable Long owner_id){
+        return new ResponseEntity<Owner>(ownerService.findById(owner_id), HttpStatus.OK);
     }
 }
 
---
+++ pet-clinic-web/src/main/java/volha/spring/newpetclinic/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package volha.spring.newpetclinic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+/*
+ *Created by olga on 12.08.2020
+ */
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig { //extends WebMvcConfigurationSupport {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/**"))
+//                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("Volha", "http://localhost:8080", "olgakochanowa@gmail.com");
+        return new ApiInfo("Pet Clinic", "Application for pet clinic",
+                "1,0", "termsOfServiceUrl", contact,
+                "license", "licenseUrl", new ArrayList());
+    }
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry){
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resource");
+//
+//        registry.addResourceHandler("/webjars/*")
+//                .addResourceLocations("classpath:/META-INF/resource/webjars");
+//    }
+}
